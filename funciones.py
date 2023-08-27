fd4722e5c82c2b36ac8f0a4e31f1febe7e6f956e
666borz666 <iloveass3333@gmail.com>
2023-08-26 22:34:44 -0600

    programa sin aux y con error en func 3

--- funciones.py
+++ funciones.py
@@ -1,4 +1,76 @@
 #Elaborado por Daniel Campos
-#Creacion
-#Ultima mod
+#Creacion 26-08-2023 3:00 pm
+#Ultima mod 26-08-2023 
 #Version 3.10.6
+
+#definicion de funciones
+#digitos pares
+#1
+def sacarPares(n):
+    """
+    F: Del número n, separa los dígitos que son pares y los muestra
+    E: Número n
+    S: Dígitos del número n que son pares
+    """
+    while n>0:
+        x=int(n%10)
+        if x%2==0:
+            print("Dígito par: ",x)
+        else:
+            print("Dígito impar:", x)
+        n=n//10
+    return ""
+
+#numero mas grande de la cifra
+#2
+def calcularMasgrande(n):
+    """
+    F: Del número n, calcula la cifra con mayor tamaño
+    E: Número n
+    S: Cifra del número n más grande
+    """
+    mayor=0
+    menor=0
+    while n>0:
+        x=int(n%10)
+        if x>mayor:
+            mayor=x
+        else:
+            menor=x
+        n=n//10
+    return mayor
+
+#multiplicar numeros impares
+#3
+def multiplicarImpares(n):
+    """
+    F: Del número n, separa los dígitos que son impares y los multiplica
+    E: Numero n
+    S: Resultado de la múltiplicacion de los dígitos impares del número n
+    """
+    while n>0:
+        x=int(n%10)
+        if x!=0:
+            multi=0
+            multi=x*x
+        n//10
+    return multi
+
+#valores de las posiciones impares
+#4
+def sacarValorPosicionesImpares(n):
+    """
+    F: Del número n, lee la cantidad de cifras que tiene y, de las cifras que son impares, muestra el número en esa posición
+    E: Número n
+    S: Dígitos en las posiciones impares
+    """
+    i=0
+    while n>0:
+        x=int(n%10)
+        if i%2==0:
+            print(x)
+        else:
+            print("")
+        i+=1
+        n=n//10
+    return ""
--- menu.py
+++ menu.py
@@ -1,4 +1,114 @@
 #Elaborado por Daniel Campos
-#Creacion
-#Ultima mod
+#Creacion 26-08-2023 3:00 pm
+#Ultima mod 26-08-2023 
 #Version 3.10.6
+
+from funciones import *
+#1
+def sacarParesAux(n):
+    """
+    F: Revisa que los datos de entrada sean los correctos
+    E: Número n
+    S: Función seleccionada
+    """
+    if isinstance(n, str)==True:
+        print("Debe ingresar únicamente números.")
+    elif n==0:
+        print("Debe ingresar números diferentes de 0.")
+    else:
+        return sacarPares(n)
+
+def opcionSacarPares(n):
+    """
+    F: Entrada y salida de datos de la funcion sacar pares
+    E: Número n
+    S: Dígitos del número n que son pares
+    """
+    print("Dígitos pares de un número.")
+    n=int(input("Digite un número diferente de 0: "))
+    print(sacarPares(n))
+ #2   
+def opcionCalcularMasgrande(n):
+    """
+    F: Entrada y salida de datos de la función calcular más grande
+    E: Número n
+    S: Cifra del número n más grande
+    """
+    print("Dígitos más grande de un número.")
+    n=int(input('Digite un numero entero positivo:'))
+    print(calcularMasgrande(n))
+    
+def calcularMasgrandeAux(n):
+    """
+    F: Revisa que los datos de entrada sean los correctos
+    E: Número n
+    S: Función seleccionada
+    """
+    return 
+#3
+def opcionMultiplicarImpares(n):
+    """
+    F: Entrada y salida de datos de la función multiplicar impares
+    E: Numero n
+    S: Resultado de la múltiplicacion de los dígitos impares del número n
+    """
+    print("Multiplicar dígitos impares de un número.")
+    n=int(input('Digite un numero entero positivo:'))
+    print(multiplicarImpares(n))
+    
+def multiplicarImparesAux(n):
+    """
+    F: Revisa que los datos de entrada sean los correctos
+    E: Número n
+    S: Función seleccionada
+    """
+    return
+#4
+def opcionSacarValorPosicionesImpares(n):
+    """
+    F: Entrada y salida de datos de la función multiplicar impares
+    E: Número n
+    S: Dígitos en las posiciones impares
+    """
+    print("Dígitos en las posiciones impares de un número")
+    n=int(input('Digite un numero entero positivo:'))
+    print(sacarValorPosicionesImpares(n))
+    
+def sacarValorPosicionesImparesAux(n):
+    """
+    F: Revisa que los datos de entrada sean los correctos
+    E: Número n
+    S: Función seleccionada
+    """
+    return
+
+#menu
+def menu():
+    """
+    Funcionamiento: De manera repetitiva, muestra el menú al usuario. 
+    Entradas: NA
+    Salidas: Resultado según lo solicitado
+    """
+    n=0
+    print ("1. Dígitos pares")
+    print ("2. Dígito más grande")
+    print ("3. Multiplicar dígitos impares")
+    print ("4. Dígitos en posiciones impares")
+    print ("0. Terminar")
+    opcion = int (input ("Escoja una opción: "))
+    if opcion >=0 and opcion <=4:
+        if opcion == 1:
+            opcionSacarPares(n)
+        elif opcion == 2 :
+            opcionCalcularMasgrande(n)
+        elif opcion == 3:
+            opcionMultiplicarImpares(n)
+        elif opcion == 4:
+            opcionSacarValorPosicionesImpares(n)
+        else:
+            return
+    else:
+        print ("Opción inválida")
+        
+#programa principal
+menu()
